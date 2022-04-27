d61560270fb1add190abcfae6b66a7027e2ad85f
Kareem El-Giushy <kareem.64123@gmail.com>
2022-04-28 00:51:02 +0200

    Make Photo Field Nullable In DB Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string("photo", 100);
+            $table->string("photo", 100)->nullable();
             $table->boolean("user_group")->default(0);
             $table->rememberToken();
             $table->timestamps();
